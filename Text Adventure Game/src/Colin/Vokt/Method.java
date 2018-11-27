39ef207ca9adb142e3b143b53b434daa9a2b02bc
colin.vokt <colin.vokt@CLC-LAB209-22.TCSD.tooelesd.org>
2018-11-27 13:59:40 -0700

    This is all the info for my TAG

---
+++ Text Adventure Game/src/Colin/Vokt/Method.java
@@ -0,0 +1,115 @@
+package Colin.Vokt;
+
+import java.util.Scanner;
+
+public class Method {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		template(input);
+
+	}
+	public static void template (Scanner input) {
+		//Assci art
+		System.out.println();
+		
+		//Tell Story
+		System.out.println();
+		
+		//promt question
+		System.out.println();
+		
+		//get user input
+		String answer = input.nextLine();
+		
+		//Check Answer
+		switch (answer) {
+		case "respond1":
+			template1(input);	
+			break;
+		case "respond2":
+			template2(input);
+			break;
+		case "respond3":
+			template3(input);
+			break;
+		}
+		
+		
+	}
+	public static void template1 (Scanner input) {
+		//Assci art
+		System.out.println();
+		
+		//Tell Story
+		System.out.println();
+		
+		//promt question
+		System.out.println();
+		
+		//get user input
+		String answer = input.nextLine();
+		
+		//Check Answer
+		switch (answer) {
+		case respond1:
+		break;
+		case respond2:
+		break;
+		case respond3:
+		break;
+		}
+		
+		
+	}
+	public static void template2 (Scanner input) {
+		//Assci art
+		System.out.println();
+		
+		//Tell Story
+		System.out.println();
+		
+		//promt question
+		System.out.println();
+		
+		//get user input
+		String answer = input.nextLine();
+		
+		//Check Answer
+		switch (answer) {
+		case respond1:
+		break;
+		case respond2:
+		break;
+		case respond3:
+		break;
+		}
+		
+		
+	}
+	public static void template3 (Scanner input) {
+		//Assci art
+		System.out.println();
+		
+		//Tell Story
+		System.out.println();
+		
+		//promt question
+		System.out.println();
+		
+		//get user input
+		String answer = input.nextLine();
+		
+		//Check Answer
+		switch (answer) {
+		case respond1:
+		break;
+		case respond2:
+		break;
+		case respond3:
+		break;
+		}
+		
+		
+	}
+}
---
+++ Text Adventure Game/src/Colin/Vokt/TAG.java
@@ -0,0 +1,43 @@
+package Colin.Vokt;
+
+import java.util.Scanner;
+
+public class TAG {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+		//Title
+		//Intro
+		System.out.println("-------------------------------------------------------------------------------------------------------");
+		System.out.println(" Its 200,000 years into the future in time of choas and intergalatic proverty"
+				+ ".\n Once all walks of life across the Milky Way Galaxy were united and worked in harameny"
+				+ ".\n Just resources were running out and the populations were growing by billions every year"
+				+ ".\n Panic became the norm the glaxay. Most races tried to bring peace back but choas consumed them"
+				+ ".\n Soon all nations wold crumble under the pressure and now, society is a real life hell"
+				+ ".\n With crime and slavery being normal. Thats where this story starts. On a moon of Neptune called Triton,"
+				+ "\n its known for its slavery and battle arena. A man named Janus, runs this place. His troops will search"
+				+ "\n the slums for fighers, these fighters are slaved and force to fight. But there is a benift to being "
+				+ "\n a figher. If a fighter can win 100 matches then they gain freedom and riches to live life how ever the want.");
+		System.out.println("-------------------------------------------------------------------------------------------------------");
+		// 1. pick character
+		
+		System.out.println("\n We have 2 fighters, First is Io hes small frail boy who weighes about 110 pounds."
+				+ " \nHe has his glorias sword that can break thru any material!. Nike comes from one of poorest slum on Triton."
+				+ " \nBut he wanted to be a figher. Hes philosfey is he has a better chance living in the arena then living on the streets with no food."
+				+ " \nHe wants to win the 100 match challenge to save his fellow slaves in the slums!.");
+		System.out.println("Io special abilites include:\n Luck \n Can go wiouth water for more then 3 days \n Hes really lucky.");
+		System.out.println("");//Picture of Io
+		System.out.println("-------------------------------------------------------------------------------------------------------");
+		
+		//dialog enters fight
+		
+		
+		
+		
+		
+	}
+
+}
